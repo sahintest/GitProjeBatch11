1dfe1b58c0ba349e9e3c6d72922c5d70d78fa6fb
sahintest <74535772+sahintest@users.noreply.github.com>
2020-11-17 14:33:01 +0300

    Update BirinciGun.java
    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,7 +2,7 @@ public class BirinciGun {
 	
 	public static void main(String[] args) {
 		System.out.println("11:05 - Birinci Gün");
-		System.out.println("11:05 - Birinci Gün 2")
+		System.out.println("11:05 - Birinci Gün-2")
 	
 	}
 }
