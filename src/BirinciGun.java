a008169e5246a2ee5742c730c2525bdb10700cf5
sahintest <74535772+sahintest@users.noreply.github.com>
2020-11-17 23:24:43 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,13 +2,13 @@ public class BirinciGun {
 	
 	public static void main(String[] args) {
 
-		System.out.println("11:57 - Birinci Gün");
-		System.out.println("11:57 - Ýkinci Commit");
-		System.out.println("11:05 - Birinci GÃ¼n");
-		System.out.println("11:05 - Birinci GÃ¼n-2");
+		System.out.println("11:57 - Birinci GÃ¼n");
+		System.out.println("11:57 - Ikinci Commit");
+		System.out.println("11:05 - Birinci Gun");
+		System.out.println("11:05 - Birinci Gun-2");
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:11 - Ikinci");
-
+		System.out.println("10:23 - github.com");
 
 }
 }
