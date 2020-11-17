37456d0a0cff04769a17ecdf345f07cb2a800b50
sahintest <74535772+sahintest@users.noreply.github.com>
2020-11-17 23:05:23 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,6 +4,9 @@ public class BirinciGun {
 		System.out.println("11:05 - Birinci Gün");
 		System.out.println("11:05 - Birinci Gün-2")
 		System.out.println("10:11 - Ikinci Gun")
+		System.out.println("10:12 - Ikinci Gun")
+		System.out.println("10:13 - Ikinci Gun")
+		System.out.println("10:14 - Ikinci Gun")
 	}
 }
 
